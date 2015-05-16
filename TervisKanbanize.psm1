b5059ff515c52cef34e7c8a70417f234ca9344d8
ChrisMagnuson <hourglasssand@gmail.com>
2015-05-16 14:31:11 -0400

    Initial commit

---
+++ TervisKanbanize.psm1
@@ -0,0 +1,12 @@
+﻿filter Mixin-TervisKanbanizeCardProperties {
+    $_ | Add-Member -MemberType ScriptProperty -Name TrackITID -Value { [int]$($this.customfields | Where name -eq "trackitid" | select -ExpandProperty value) }
+    $_ | Add-Member -MemberType ScriptProperty -Name PositionInt -Value { [int]$this.position }
+    $_ | Add-Member -MemberType ScriptProperty -Name PriorityInt -Value { 
+        switch($this.color) {
+            "#cc1a33" {1} #Red for priority 1
+            "#f37325" {2} #Orange for priority 2
+            "#77569b" {3} #Purple for priority 3
+            "#067db7" {4} #Blue for priority 4
+        }
+    }
+}

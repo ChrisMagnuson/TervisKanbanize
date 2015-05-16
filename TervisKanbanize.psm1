f68e7fb4f73acabcd1a8a92717f197ece9501db1
ChrisMagnuson <hourglasssand@gmail.com>
2015-05-16 17:30:08 -0400

    Added new Move-CompletedCardsThatHaveAllInformationToArchive function to help automate card archival.

--- TervisKanbanize.psm1
+++ TervisKanbanize.psm1
@@ -10,3 +10,33 @@
         }
     }
 }
+
+function Move-CompletedCardsThatHaveAllInformationToArchive {
+    $KanbanizeBoards = Get-KanbanizeProjectsAndBoards
+    $HelpDeskBoardID = $KanbanizeBoards.projects.boards | where name -EQ "Help Desk Technician Process" | select -ExpandProperty ID
+    $TriageBoardID = $KanbanizeBoards.projects.boards | where name -EQ "Help Desk Triage Process" | select -ExpandProperty ID
+
+    $TechnicianProcessCards = Get-KanbanizeAllTasks -BoardID $HelpDeskBoardID
+    $TechnicianProcessCards | Add-Member -MemberType NoteProperty -Name BoardID -Value $HelpDeskBoardID
+    
+    $TriageProcessCards = Get-KanbanizeAllTasks -BoardID $TriageBoardID
+    $TriageProcessCards | Add-Member -MemberType NoteProperty -Name BoardID -Value $TriageBoardID
+
+    $Cards = $TechnicianProcessCards + $TriageProcessCards
+
+    $Cards | Mixin-TervisKanbanizeCardProperties
+
+    $OpenTrackITWorkOrders = get-TrackITWorkOrders
+
+    $CardsThatCanBeArchived = $Cards | 
+    where columnpath -Match "Done" |
+    where type -ne "None" |
+    where assignee -NE "None" |
+    where color -in ("#cc1a33","#f37325","#77569b","#067db7") |
+    where TrackITID |
+    where TrackITID -NotIn $($OpenTrackITWorkOrders.woid)
+
+    foreach ($Card in $CardsThatCanBeArchived){
+        Move-KanbanizeTask -BoardID $Card.BoardID -TaskID $Card.TaskID -Column "Archive"
+    }
+}
